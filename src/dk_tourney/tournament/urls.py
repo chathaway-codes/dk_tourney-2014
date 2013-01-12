3728d383093bf0e19251d6dfb19bf64d6f2576c5
Charles H <chathaway@logrit.com>
2013-01-11 21:56:46 -0500

    Added the game listings

--- src/dk_tourney/tournament/urls.py
+++ src/dk_tourney/tournament/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 
-from tournament.views import TournamentListView
+from tournament.views import TournamentListView, GameListView, GameDetailView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -8,5 +8,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^tournaments/$', TournamentListView.as_view(), {}, 'tournament_list'),
+    url(r'^games/$', GameListView.as_view(), {}, 'game_list'),
+
+    url(r'^games/(?P<pk>\d+)/$', GameDetailView.as_view(), {}, 'game_detail'),
 )
 
--- src/dk_tourney/tournament/views.py
+++ src/dk_tourney/tournament/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 
-from tournament.models import Tournament
+from tournament.models import Tournament, Game
 
 class TournamentListView(ListView):
     model = Tournament
@@ -11,3 +11,12 @@ class TournamentListView(ListView):
 class TournamentDetailedView(DetailView):
     model = Tournament
     context_object_name = 'list'
+
+
+class GameListView(ListView):
+    model = Game
+    context_object_name = 'list'
+
+class GameDetailView(DetailView):
+    model = Game
+    context_object_name = 'game'
