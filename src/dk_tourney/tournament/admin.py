d24b99277193889c1de74527fcd26e12f1400131
Charles H <chathaway@logrit.com>
2013-01-11 22:32:52 -0500

    You can not show ineterest in a game.

--- src/dk_tourney/tournament/admin.py
+++ src/dk_tourney/tournament/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from tournament.models import Game, Team, Tournament
+from tournament.models import Game, Team, Tournament, Player
 
 admin.site.register(Game)
 admin.site.register(Team)
 admin.site.register(Tournament)
+admin.site.register(Player)
--- src/dk_tourney/tournament/urls.py
+++ src/dk_tourney/tournament/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 
-from tournament.views import TournamentListView, GameListView, GameDetailView
+from tournament.views import TournamentListView, GameListView, GameDetailView, interest_in_game
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -11,5 +11,6 @@ urlpatterns = patterns('',
     url(r'^games/$', GameListView.as_view(), {}, 'game_list'),
 
     url(r'^games/(?P<pk>\d+)/$', GameDetailView.as_view(), {}, 'game_detail'),
+    url(r'^games/(?P<pk>\d+)/interested$', interest_in_game, {}, 'game_interest_added'),
 )
 
