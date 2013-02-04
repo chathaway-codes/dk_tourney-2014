7df507a9a6f0362716958bf0a69c1025261824a8
Charles H <chathaway@logrit.com>
2013-02-04 16:43:08 -0500

    Minor typo

--- src/dk_tourney/dk_tourney/views.py
+++ src/dk_tourney/dk_tourney/views.py
@@ -7,7 +7,7 @@ from tournament.models import Game, Tournament, Player
 def home(request):
     games = Game.objects.all()
     total_games = Game.objects.all().count()
-    total_tournaments = Game.objects.all().count()
+    total_tournaments = Tournament.objects.all().count()
     total_players = Player.objects.all().count()
 
     return render_to_response('home.html', {"games": games, "form": AuthenticationForm(), "total_players": total_players, "total_games": total_games, "total_tournaments": total_tournaments}, context_instance=RequestContext(request))
